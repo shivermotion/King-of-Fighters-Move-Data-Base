4d5c81fa5e2401db66f6398bcd7e6e799f679d69
Jason Day <jason.day.blue@gmail.com>
2022-02-26 22:00:31 -0600

    added paypal button

--- src/App.js
+++ src/App.js
@@ -18,10 +18,46 @@ function App() {
 						/>
 						KOFmdb
 					</a>
+					<form
+						action="https://www.paypal.com/donate"
+						method="post"
+						target="_top"
+					>
+						<input
+							type="hidden"
+							name="business"
+							value="527GL73ZVALQ8"
+						/>
+						<input
+							type="hidden"
+							name="no_recurring"
+							value="0"
+						/>
+						<input
+							type="hidden"
+							name="currency_code"
+							value="USD"
+						/>
+						<input
+							type="image"
+							src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif"
+							border="0"
+							name="submit"
+							title="PayPal - The safer, easier way to pay online!"
+							alt="Donate with PayPal button"
+						/>
+						<img
+							alt=""
+							border="0"
+							src="https://www.paypal.com/en_US/i/scr/pixel.gif"
+							width="1"
+							height="1"
+						/>
+					</form>
 				</div>
 			</nav>
 			<p class="text-center">
-				<h1>FIGHTERS</h1>
+				<h1>FIGHTERSasdfasdf</h1>
 			</p>
 			{/*CHARACTER LIST */}
 			<div
